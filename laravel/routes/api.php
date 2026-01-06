ea42ececa4d6bcc4172b75ccbce1acefead5375d
cauanvital <cauanvital74@gmail.com>
2026-01-06 18:21:01 -0300

    feat: agora a api está funcionando

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,5 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
 use App\Http\Controllers\NfeController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
 // Apenas para definir o endpoint da API
 Route::get('/nfe/{id}', [NfeController::class, 'show']);
+
