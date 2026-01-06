fb4997e85b6889b4066ec4ff017966f7bde0acc2
cauanvital <cauanvital74@gmail.com>
2026-01-06 19:36:31 -0300

    feat: fazendo funcionar com o banco de fato

--- app/Http/Controllers/NfeController.php
+++ app/Http/Controllers/NfeController.php
@@ -10,37 +10,43 @@ class NfeController extends Controller
 {
     public function show($id)
     {
-        // Simulação do resultado do Inner Join (conforme solicitado)
-        $result = [
-            ["id_nfe" => 1, "nr_nfe" => 9998, "id_produto" => 1, "ds_produto" => "Produto 1", "nr_quantidade" => 1],
-            ["id_nfe" => 1, "nr_nfe" => 9998, "id_produto" => 2, "ds_produto" => "Produto 2", "nr_quantidade" => 2],
-            ["id_nfe" => 1, "nr_nfe" => 9998, "id_produto" => 3, "ds_produto" => "Produto 3", "nr_quantidade" => 1]
-        ];
+        // Query com join no banco
+        $result = DB::table('nfes')
+            ->join('produtos', 'nfes.id', '=', 'produtos.nfe_id')
+            ->select(
+                'nfes.id as id_nfe',
+                'nfes.nr_nfe',
+                'produtos.id as id_produto',
+                'produtos.ds_produto',
+                'produtos.nr_quantidade'
+            )
+            ->where('nfes.id', $id)
+            ->get();
 
         // Transformação dos dados pra JSON
         if (empty($result)) {
             return response()->json(['message' => 'NFe não encontrada'], 404);
         }
 
-        // Agrupar dados
-        $formatado = [
-            "id_nfe" => $result[0]['id_nfe'],
-            "nr_nfe" => $result[0]['nr_nfe'],
-            "nr_quantidade_produto" => 0,
+        // Monta o JSON
+        $json = [
+            "id_nfe" => $result[0]->id_nfe,
+            "nr_nfe" => $result[0]->nr_nfe,
+            "nr_quantidade_produto" => 0, // Inicia em zero para somar depois
             "produtos" => []
         ];
 
         foreach ($result as $item) {
-            $formatado['produtos'][] = [
-                "id_produto" => $item['id_produto'],
-                "ds_produto" => $item['ds_produto'],
-                "nr_quantidade" => $item['nr_quantidade'],
+            $json['produtos'][] = [
+                "id_produto" => $item->id_produto,
+                "ds_produto" => $item->ds_produto,
+                "nr_quantidade" => $item->nr_quantidade
             ];
-            // Soma a quantidade total
-            $formatado['nr_quantidade_produto'] += $item['nr_quantidade'];
+            // Soma a quantidade total de produtos
+            $json['nr_quantidade_produto'] += $item->nr_quantidade;
         }
 
-        // Exibindo com var_dump ou retornando JSON
-        return response()->json($formatado);
+        // Exibindo retornando JSON
+        return response()->json($json);
     }
 }
