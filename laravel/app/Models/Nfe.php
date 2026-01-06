6d778975a9dbf8a8d79f924a7b7eb2464fe1e619
cauanvital <cauanvital74@gmail.com>
2026-01-06 18:13:22 -0300

    refact: refatoração no model da Nfe

--- laravel/app/Models/Nfe.php
+++ laravel/app/Models/Nfe.php
@@ -5,13 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Nfe extends Model {
-
     protected $table = 'nfes';
-
     protected $fillable = ['nr_nfe'];
 
     public function produtos() {
         return $this->hasMany(Produto::class);
     }
-
 }
